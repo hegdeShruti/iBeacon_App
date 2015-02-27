605d47860548b03cf4c0c401fc2c4f0b853305aa
shruti.hegde <shruti.hegde@tavant.com>
2015-02-27 10:52:02 +0530

    initial checkin
    creating app repo

---
+++ iBeacon_Retail/iBeacon_Retail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBeacon_Retail
+//
+//  Created by ShrutiHegde on 2/27/15.
+//  Copyright (c) 2015 TAVANT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iBeacon_Retail/iBeacon_Retail/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iBeacon_Retail
+//
+//  Created by ShrutiHegde on 2/27/15.
+//  Copyright (c) 2015 TAVANT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
